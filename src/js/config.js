c490b32f7b98b40ea31d3dba349cdda32cdee0e2
Alexei Plokhikh <126824464+Alexplokhikh@users.noreply.github.com>
2025-03-06 18:31:41 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'd858fa68-8f1c-47aa-8f49-3da3058e1b08';
